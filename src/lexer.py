bd20d542eb1b47c59d7f593aa57917f6c7eb7b83
NghiaLe4575 <160830150+NghiaLe4575@users.noreply.github.com>
2026-05-20 10:05:56 +0700

    Update lexer.py

--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,6 @@
 #import TokenType
 from src.token import TokenType, KEYWORDS, Token
+
 class Lexer:
     def __init__(self, Istring : str):
         self.input = Istring
@@ -37,18 +38,19 @@ class Lexer:
             return self.read_char_literal()
         
         if self.char == '/':
-            # If the NEXT character is / or *, it's a comment!
-            if self.peek_char() == '/' or self.peek_char() == '*':
+            # Check the actual lookahead character safely
+            next_ch = self.peek_char()
+            if next_ch == '/' or next_ch == '*':
                 self.skip_comment()
                 return self.nextToken()
             
         # Try to parse an operator/delimiter first
         op_token = self.peek_operator()
         if op_token is not None:
-            token = op_token
+            return op_token
             
         else :
-            if self.isLetter(self.char) or self.char == "_":
+            if self.isLetter(self.char):
                 literal = self.read_identifier()
                 # Check if it's a Go keyword, otherwise it's just an IDENTifier
                 token_type = KEYWORDS.get(literal, TokenType.IDENT)
@@ -67,10 +69,10 @@ class Lexer:
 
     def read_identifier(self) -> str:
         pos = self.curCharPos
-        #identifiers can have numbers after the first letter
         while self.isLetter(self.char) or self.char.isdigit():
             self.read_char()
         return self.input[pos:self.curCharPos]
+
     def skip_comment(self):
         # SINGLE LINE COMMENTS (//)
         if self.peek_char() == '/':
@@ -91,43 +93,34 @@ class Lexer:
                     break
             
     def read_string(self) -> Token:
-        #currently sitting ON the opening '"'
         start_pos = self.curCharPos
         
         while True:
             self.read_char()
-
             if self.char == '\\':
                 self.read_char()
-                continue # Jump back to the top of the loop
+                continue 
                 
-            # Stop Condition
             if self.char == '"' or self.char == '\0':
                 break
                 
         self.read_char()
-        
-        # Slice the entire string (This will INCLUDE the quotes!)
         string_literal = self.input[start_pos : self.curCharPos]
-        
         return Token(TokenType.STRING, string_literal)
         
     def read_number(self) -> Token:
         pos = self.curCharPos
         is_float = False
 
-        # Read the base integer part
         while self.char.isdigit():
             self.read_char()
 
-        # Check for a decimal point
         if self.char == '.':
             is_float = True
             self.read_char()
             while self.char.isdigit():
                 self.read_char()
 
-        # Check for Scientific Notation (e or E)
         if self.char in ('e', 'E'):
             is_float = True
             self.read_char()
@@ -136,16 +129,13 @@ class Lexer:
             while self.char.isdigit():
                 self.read_char()
 
-        # Check for Imaginary number (i)
         is_imaginary = False
         if self.char == 'i':
             is_imaginary = True
-            self.read_char() # Consume the 'i'
+            self.read_char() 
 
-        # Slice the final gathered string
         number_literal = self.input[pos:self.curCharPos]
 
-        # Determine the correct token type
         if is_imaginary:
             return Token(TokenType.IMAG, number_literal)
         elif is_float:
@@ -156,27 +146,29 @@ class Lexer:
     def peek_char(self) -> str:
         if self.nextCharPos >= len(self.input):
             return '\0'
-        else:
-            return self.input[self.nextCharPos]
+        return self.input[self.nextCharPos]
         
     def peek_operator(self):
-        # try to grab 3 characters
-        peek_three = self.input[self.curCharPos : self.nextCharPos + 2]
-    
-        if peek_three in ("<<=", ">>=", "...", "&^="):
-            self.read_char() # Advance past char 2
-            self.read_char() # Advance past char 3
-            return Token(TokenType(peek_three), peek_three)
+        # Explicit length boundaries based cleanly on curCharPos
+        peek_three = self.input[self.curCharPos : self.curCharPos + 3]
+        if len(peek_three) == 3 and peek_three in ("<<=", ">>=", "...", "&^="):
+            literal = peek_three
+            self.read_char() # Consume 1st extra
+            self.read_char() # Consume 2nd extra
+            self.read_char() # Consume the final character of the token
+            return Token(TokenType(literal), literal)
 
-        # Try to grab 2 characters
-        peek_two = self.input[self.curCharPos : self.nextCharPos + 1]
-    
-        if peek_two in ("<<", ">>", "&^", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "&&", "||", "<-", "++", "--", "==", "!=", "<=", ">=", ":="):
-            self.read_char() # Advance past char 2
-            return Token(TokenType(peek_two), peek_two)
+        peek_two = self.input[self.curCharPos : self.curCharPos + 2]
+        if len(peek_two) == 2 and peek_two in ("<<", ">>", "&^", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "&&", "||", "<-", "++", "--", "==", "!=", "<=", ">=", ":="):
+            literal = peek_two
+            self.read_char() # Consume 1st extra
+            self.read_char() # Consume the final character of the token
+            return Token(TokenType(literal), literal)
 
         if self.char in ("+", "-", "*", "/", "%", "&", "|", "^", "<", ">", "=", "!", "(", "[", "{", ",", ".", ")", "]", "}", ";", ":"):
-            return Token(TokenType(self.char), self.char)
+            literal = self.char
+            self.read_char() # MUST consume the single character!
+            return Token(TokenType(literal), literal)
 
         return None
                 
@@ -185,7 +177,6 @@ class Lexer:
         
         while True:
             self.read_char()
-
             if self.char == '\\':
                 self.read_char()
                 continue 
@@ -194,7 +185,5 @@ class Lexer:
                 break
                 
         self.read_char()
-        
         char_literal = self.input[start_pos : self.curCharPos]
-        
         return Token(TokenType.CHAR, char_literal)
